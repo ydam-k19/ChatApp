6d081d7238f6f067444504a0b42f90ff088c74b3
ydam <ydamk15nbk@gmail.com>
2021-11-14 10:30:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/chatapp/activities/SignInActivity.java
@@ -0,0 +1,29 @@
+package com.example.chatapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.chatapp.databinding.ActivitySignInBinding;
+
+
+public class SignInActivity extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+    private void setListeners(){
+        binding.textCreateNewAccount.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(),SignUpActivity.class)));
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/chatapp/utilities/Constants.java
@@ -0,0 +1,12 @@
+package com.example.chatapp.utilities;
+
+public class Constants {
+    public static String KEY_COLLECTION_USERS = "users";
+    public static String KEY_NAME = "name";
+    public static String KEY_EMAIL = "email";
+    public static String KEY_PASSWORD = "password";
+    public static String KEY_PREFERENCE_NAME = "chatAppPreference";
+    public static String KEY_IS_SIGNED_IN = "isSignedIn";
+    public static String KEY_USER_ID = "userId";
+    public static String KEY_IMAGE = "image";
+}
